9b59513b540cd4e055f675582d426096dedd09e6
mattlogan <matt.b.logan@gmail.com>
2014-06-04 08:49:40 -0700

    Moved OnOverlayDragListener into its own file

---
+++ library/src/main/java/com/matthewlogan/selectablemapfragment/library/OnOverlayDragListener.java
@@ -0,0 +1,10 @@
+package com.matthewlogan.selectablemapfragment.library;
+
+import com.google.android.gms.maps.model.LatLngBounds;
+
+/**
+ * Created by matthewlogan on 6/4/14.
+ */
+public interface OnOverlayDragListener {
+    public void onOverlayDrag(LatLngBounds latLngBounds);
+}
